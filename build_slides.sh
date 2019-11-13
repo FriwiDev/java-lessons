06e0c469519a8ab736f5308b120622f0af89c4cb
Fritz Windisch <s9395719@msx.tu-dresden.de>
2019-11-13 17:54:05 +0100

    Week 2 initial commit

--- build_slides.sh
+++ build_slides.sh
@@ -4,3 +4,17 @@ cd latex/slides
 xelatex 01-introduction.tex
 biber 01-introduction
 xelatex 01-introduction.tex
+xelatex 02-oop.tex
+biber 02-oop
+xelatex 02-oop.tex
+xelatex 03-inheritance.tex
+biber 03-inheritance
+xelatex 03-inheritance.tex
+
+cd ../exercises
+xelatex 01-temperature.tex
+xelatex 02a-library-part-1.tex
+xelatex 02b-fibonacci.tex
+xelatex 02c-references.tex
+xelatex 03a-library-part-2.tex
+xelatex 03b-multiplication-table.tex
